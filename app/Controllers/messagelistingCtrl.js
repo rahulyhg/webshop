8e15ca982314d0f7d657d25ff29c626dac086f64
nits2 <arunavaguha@natitsolved.com>
2018-05-04 19:59:01 +0530

    04_05

--- app/Controllers/messagelistingCtrl.js
+++ app/Controllers/messagelistingCtrl.js
@@ -11,8 +11,22 @@ $scope.checkboxstr=[];
  $scope.user.brand=[];
  $scope.checkboxstr2=[];
  $scope.user.shop=[];
- var userInfo = JSON.parse($window.localStorage["userInfo"]);	
-$scope.user_id=userInfo.user_id;
+if ($window.localStorage["userInfo"]) {
+var userInfo = JSON.parse($window.localStorage["userInfo"]);	
+	$scope.user_id=userInfo.user_id;
+        $scope.getCurrentUserType();
+}
+else {
+	var userInfo={};
+	userInfo.user_id ="";
+}
+
+$scope.isform1 =0;
+	$scope.form1 = function(user) {
+
+	$scope.isform1 =1;
+
+}
 
 userService.listproductMessages($scope.user_id).then(function(response) {
          // alert('hii');
