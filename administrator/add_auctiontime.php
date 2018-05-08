c3a81d812485a3a7553dc6b4f84d977ab128f702
nits2 <arunavaguha@natitsolved.com>
2018-05-08 14:46:25 +0530

    08_05_2:46

--- administrator/add_auctiontime.php
+++ administrator/add_auctiontime.php
@@ -9,43 +9,51 @@ if (isset($_REQUEST['submit'])) {
     $end_time = isset($_POST['end_time']) ? $_POST['end_time'] : '';
 
 
-
-    $fields = array(
-        'start_time' => mysqli_real_escape_string($con, $actual_word),
-        'end_time' => mysqli_real_escape_string($con, $end_time),
-    );
-
-    $fieldsList = array();
-    foreach ($fields as $field => $value) {
-        $fieldsList[] = '`' . $field . '`' . '=' . "'" . $value . "'";
-    }
-
-    if ($_REQUEST['action'] == 'edit') {
-        $editQuery = "UPDATE `webshop_auctiontimes` SET " . implode(', ', $fieldsList)
-                . " WHERE `id` = '" . mysqli_real_escape_string($con, $_REQUEST['id']) . "'";
-        // exit;
-
-        if (mysqli_query($con, $editQuery)) {
+    $mydatetime = date('Y-m-d H:i:s', strtotime($mydatetime));
 
 
-            $_SESSION['msg'] = "Category Updated Successfully";
-        } else {
-            $_SESSION['msg'] = "Error occuried while updating Category";
+
+    for ($k = 0; $k <= count($start_time); $k++) {
+        $start_time[$k] = date('Y-m-d H:i:s', strtotime($start_time[$k]));
+        $end_time[$k] = date('Y-m-d H:i:s', strtotime($end_time[$k]));
+
+        $fields = array(
+            'start_time' => mysqli_real_escape_string($con, $start_time[$k]),
+            'end_time' => mysqli_real_escape_string($con, $end_time[$k]),
+        );
+
+        $fieldsList = array();
+        foreach ($fields as $field => $value) {
+            $fieldsList[] = '`' . $field . '`' . '=' . "'" . $value . "'";
         }
 
-        header('Location:list_english.php');
-        exit();
-    } else {
+        if ($_REQUEST['action'] == 'edit') {
+            $editQuery = "UPDATE `webshop_auctiontimes` SET " . implode(', ', $fieldsList)
+                    . " WHERE `id` = '" . mysqli_real_escape_string($con, $_REQUEST['id']) . "'";
+            // exit;
 
-        $insertQuery = "INSERT INTO `webshop_auctiontimes` (`" . implode('`,`', array_keys($fields)) . "`)"
-                . " VALUES ('" . implode("','", array_values($fields)) . "')";
-
-        mysqli_query($con, $insertQuery);
-        $last_id = mysqli_insert_id($con);
+            if (mysqli_query($con, $editQuery)) {
 
 
-        header('Location:list_english.php');
-        exit();
+                $_SESSION['msg'] = "Category Updated Successfully";
+            } else {
+                $_SESSION['msg'] = "Error occuried while updating Category";
+            }
+
+            header('Location:list_english.php');
+            exit();
+        } else {
+
+            $insertQuery = "INSERT INTO `webshop_auctiontimes` (`" . implode('`,`', array_keys($fields)) . "`)"
+                    . " VALUES ('" . implode("','", array_values($fields)) . "')";
+
+            mysqli_query($con, $insertQuery);
+            $last_id = mysqli_insert_id($con);
+
+
+            header('Location:list_english.php');
+            exit();
+        }
     }
 }
 
@@ -128,22 +136,23 @@ if ($_REQUEST['action'] == 'edit') {
                         <div class="widget-body">
                             <!-- BEGIN FORM-->
                             <form class="form-horizontal" method="post" enctype="multipart/form-data">
-
-                                <div class="control-group">
-                                    <label class="control-label">Start Time </label>
-                                    <div class="controls">
-                                        <input type="text" class="form-control" placeholder="Start Time" value="<?php echo $categoryRowset['actual_word']; ?>" name="actual_word" required>
+                                <div class="input_fields_wrap">
+                                    <div class="control-group">
+                                        <label class="control-label">Start Time </label>
+                                        <div class="controls">
+                                            <input type="text" class="form-control timepicker" placeholder="Start Time" value="<?php echo $categoryRowset['start_time']; ?>" name="start_time[]" required>
+                                        </div>
                                     </div>
-                                </div>
 
 
-                                <div class="control-group">
-                                    <label class="control-label">End Time</label>
-                                    <div class="controls">
-                                        <input type="text" class="form-control" placeholder="End Time" value="<?php echo $categoryRowset['transleted_word']; ?>" name="transleted_word" required>
+                                    <div class="control-group">
+                                        <label class="control-label">End Time</label>
+                                        <div class="controls">
+                                            <input type="text" class="form-control timepicker" placeholder="End Time" value="<?php echo $categoryRowset['end_time']; ?>" name="end_time[]" required>
+                                        </div>
                                     </div>
+                                    <button class="add_field_button">Add More Fields</button>
                                 </div>
-
 
                                 <div class="form-actions">
                                     <button type="submit" class="btn blue" name="submit"><i class="icon-ok"></i> Save</button>
@@ -189,7 +198,8 @@ if ($_REQUEST['action'] == 'edit') {
 <script src="js/excanvas.js"></script>
 <script src="js/respond.js"></script>
 <![endif]-->
-
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
 <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js" type="text/javascript"></script>
 <script src="js/jquery.sparkline.js" type="text/javascript"></script>
 <script src="assets/chart-master/Chart.js"></script>
@@ -227,6 +237,57 @@ if ($_REQUEST['action'] == 'edit') {
             }
         });
     }
+</script>
+<script>
+
+
+
+
+
+
+    $(document).ready(function () {
+        var max_fields = 10; //maximum input boxes allowed
+        var wrapper = $(".input_fields_wrap"); //Fields wrapper
+        var add_button = $(".add_field_button"); //Add button ID
+        var field = '   <div><div class="control-group">  ' +
+                '                                           <label class="control-label">Start Time </label>  ' +
+                '                                           <div class="controls">  ' +
+                '                                               <input type="text" class="form-control timepicker" placeholder="Start Time" value="<?php echo $categoryRowset["start_time"]; ?>" name="start_time[]" required>  ' +
+                '                                           </div>  ' +
+                '                                       </div>  ' +
+                '     ' +
+                '     ' +
+                '                                       <div class="control-group">  ' +
+                '                                           <label class="control-label">End Time</label>  ' +
+                '                                           <div class="controls">  ' +
+                '                                               <input type="text" class="form-control timepicker" placeholder="End Time" value="<?php echo $categoryRowset["end_time"]; ?>" name="end_time" required>  ' +
+                '</div> </div><a href="#" class="remove_field">Remove</a> ';
+
+        var x = 1; //initlal text box count
+        $(add_button).click(function (e) { //on add input button click
+            e.preventDefault();
+            if (x < max_fields) { //max input box allowed
+                x++; //text box increment
+                $(wrapper).append(field); //add input box
+
+
+            }
+        });
+
+        $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+            e.preventDefault();
+            $(this).parent('div').remove();
+            x--;
+        })
+    });
+    $('.timepicker').timepicker({
+        timeFormat: 'h:mm p',
+        interval: 60,
+        dynamic: false,
+        dropdown: true,
+        scrollbar: true
+    });
+
 </script>
 <!-- END JAVASCRIPTS -->   
 </body>
