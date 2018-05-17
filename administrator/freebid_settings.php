1bb20bf7a66d513823ff8ae3ed9eba8a1e76ded9
nitspalash <palash@natitsolved.com>
2018-05-17 19:29:32 +0530

    palash 1

---
+++ administrator/freebid_settings.php
@@ -0,0 +1,302 @@
+<?php
+include_once('includes/session.php');
+include_once("includes/config.php");
+include_once("includes/functions.php");
+
+
+     //$pid=$_REQUEST['id'];
+    $sql2="SELECT * FROM `webshop_sitesettings` where id='1'"; 
+    $res=mysqli_query($con,$sql2);
+    $row=mysqli_fetch_array($res);
+
+    if(isset($_REQUEST['submit']))
+  {
+   //$email=$_REQUEST['email'];
+   
+   $free_bid = isset($_POST['free_bid']) ? $_POST['free_bid'] : '';
+   //$publishable_key = isset($_POST['publishable_key']) ? $_POST['publishable_key'] : '';
+   
+   $fields = array('free_bid' => mysqli_real_escape_string($con,$free_bid)
+                  );
+     $fieldsList = array();
+    foreach ($fields as $field => $value) {
+      $fieldsList[] = '`' . $field . '`' . '=' . "'" . $value . "'";
+    }
+   // print_r($fieldsList);exit;
+   $editQuery = "UPDATE `webshop_sitesettings` SET " . implode(', ', $fieldsList)
+      . " WHERE `id` = '1'";
+
+    if (mysqli_query($con,$editQuery)) {
+      $_SESSION['msg'] = "Free Bid Updated Successfully";
+    }
+    else {
+      $_SESSION['msg'] = "Error occuried while updating Free Bid";
+    }
+
+    header('Location: freebid_settings.php');
+    exit();
+    
+  
+  }
+//print_r($row);
+// if(isset($_REQUEST['submit']))
+
+// {
+
+// //echo "aa"; exit;
+
+//   $secret_key = isset($_POST['secret_key']) ? $_POST['secret_key'] : '';
+
+//   $publishable_key = isset($_POST['publishable_key']) ? $_POST['publishable_key'] : '';
+
+//   //echo $name;
+//   //echo "<br>";
+//   //echo $link;
+
+
+  
+
+
+
+//    $fields = array(
+
+//     'secret_key' => mysql_real_escape_string($secret_key),
+
+//     'publishable_key' => mysql_real_escape_string($publishable_key)
+
+//     );
+
+
+
+//     $fieldsList = array();
+
+//     foreach ($fields as $field => $value) {
+
+//       $fieldsList[] = '`' . $field . '`' . '=' . "'" . $value . "'";
+
+//     }
+
+           
+
+//    if($_REQUEST['action']=='edit')
+
+//     {     
+
+//     $editQuery = "UPDATE `webshop_tbladmin` SET " . implode(', ', $fieldsList)
+
+//       . " WHERE `id` = '" . mysql_real_escape_string($_REQUEST['id']) . "'";
+
+
+
+//     if (mysql_query($editQuery)) {
+
+//     $_SESSION['msg'] = "Key Updated Successfully";
+
+//     }
+
+//     else {
+
+//       $_SESSION['msg'] = "Error occuried while updating Key";
+
+//     }
+
+
+
+//     header('Location:payment_settings.php');
+
+//     exit();
+
+  
+
+//    }
+
+//    else
+
+//    {
+
+   
+
+//    $addQuery = "INSERT INTO `webshop_tbladmin` (`" . implode('`,`', array_keys($fields)) . "`)"
+
+//       . " VALUES ('" . implode("','", array_values($fields)) . "')";
+
+      
+
+//       //exit;
+
+//     mysql_query($addQuery);
+
+
+
+//     header('Location:list_category.php');
+
+//     exit();
+
+  
+
+//    }
+
+        
+
+        
+
+// }
+  
+?>
+
+ <!-- Header Start -->
+<?php include ("includes/header.php"); ?>
+<!-- Header End -->
+ <!-- BEGIN CONTAINER -->
+   <div id="container" class="row-fluid">
+      <!-- BEGIN SIDEBAR -->
+
+    <?php include("includes/left_sidebar.php"); ?>
+
+      <!-- END SIDEBAR -->
+      <!-- BEGIN PAGE -->
+      <div id="main-content">
+         <!-- BEGIN PAGE CONTAINER-->
+         <div class="container-fluid">
+            <!-- BEGIN PAGE HEADER-->
+            <div class="row-fluid">
+               <div class="span12">
+                   <!-- BEGIN THEME CUSTOMIZER-->
+                   <div id="theme-change" class="hidden-phone">
+                       <i class="icon-cogs"></i>
+                        <span class="settings">
+                            <span class="text">Theme Color:</span>
+                            <span class="colors">
+                                <span class="color-default" data-style="default"></span>
+                                <span class="color-green" data-style="green"></span>
+                                <span class="color-gray" data-style="gray"></span>
+                                <span class="color-purple" data-style="purple"></span>
+                                <span class="color-red" data-style="red"></span>
+                            </span>
+                        </span>
+                   </div>
+                   <!-- END THEME CUSTOMIZER-->
+                  <!-- BEGIN PAGE TITLE & BREADCRUMB-->
+                   <h3 class="page-title">
+                     Freebid settings
+                   </h3>
+                   <ul class="breadcrumb">
+                       <li>
+                           <a href="#">Home</a>
+                           <span class="divider">/</span>
+                       </li>
+                       <li>
+                           <a href="#">Freebid settings</a>
+                           <span class="divider">/</span>
+                       </li>
+                       
+                     
+                       
+
+                       
+                       
+                   </ul>
+                   <!-- END PAGE TITLE & BREADCRUMB-->
+               </div>
+            </div>
+            <!-- END PAGE HEADER-->
+            <!-- BEGIN PAGE CONTENT-->
+            <div class="row-fluid">
+                <div class="span12">
+                    <!-- BEGIN SAMPLE FORMPORTLET-->
+                    <div class="widget green">
+                        <div class="widget-title">
+                            <h4><i class="icon-reorder"></i>Freebid Settings</h4>
+                            <span class="tools">
+                            <a href="javascript:;" class="icon-chevron-down"></a>
+                            <a href="javascript:;" class="icon-remove"></a>
+                            </span>
+                        </div>
+                        <div class="widget-body">
+                            <!-- BEGIN FORM-->
+                          <form action="freebid_settings.php" class="form-horizontal" method="post"  enctype="multipart/form-data">
+
+                           
+                           
+                                
+                                <div class="control-group">
+                                <label class="control-label">Free Bid</label>
+                                <div class="controls">
+                                <input type="text" class="form-control" placeholder="Enter text" value="<?php echo $row['free_bid'];?>" name="free_bid" >
+                                </div>
+                                </div>
+                                
+                                <!-- <div class="control-group">
+                                <label class="control-label">Stripe Publishable Key</label>
+                                <div class="controls">
+                                <input type="text" class="form-control" placeholder="Enter text" value="<?php echo $row['publishable_key'];?>" name="publishable_key" >
+                                </div>
+                                </div>
+                                 -->
+                              
+                              
+                                <div class="form-actions">
+                                    <button type="submit" class="btn blue" name="submit"><i class="icon-ok"></i> Save</button>
+                                    <button type="reset" class="btn"><i class=" icon-remove"></i> Cancel</button>
+                                </div>
+                            </form>
+                            <!-- END FORM-->
+                        </div>
+                    </div>
+                    <!-- END SAMPLE FORM PORTLET-->
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12">
+                   
+                </div>
+            </div>
+
+            <!-- END PAGE CONTENT-->
+         </div>
+         <!-- END PAGE CONTAINER-->
+      </div>
+      <!-- END PAGE -->
+   </div>
+   <!-- END CONTAINER -->
+
+   <!-- Footer Start -->
+
+   <?php include("includes/footer.php"); ?>
+
+   <!-- Footer End -->
+    <!-- BEGIN JAVASCRIPTS -->
+   <!-- Load javascripts at bottom, this will reduce page load time -->
+   <script src="js/jquery-1.8.3.min.js"></script>
+   <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+   <script type="text/javascript" src="assets/jquery-slimscroll/jquery-ui-1.9.2.custom.min.js"></script>
+   <script type="text/javascript" src="assets/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+   <script src="assets/fullcalendar/fullcalendar/fullcalendar.min.js"></script>
+   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+   <!-- ie8 fixes -->
+   <!--[if lt IE 9]>
+   <script src="js/excanvas.js"></script>
+   <script src="js/respond.js"></script>
+   <![endif]-->
+
+   <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js" type="text/javascript"></script>
+   <script src="js/jquery.sparkline.js" type="text/javascript"></script>
+   <script src="assets/chart-master/Chart.js"></script>
+   <script src="js/jquery.scrollTo.min.js"></script>
+
+
+   <!--common script for all pages-->
+   <script src="js/common-scripts.js"></script>
+
+   <!--script for this page only-->
+
+   <script src="js/easy-pie-chart.js"></script>
+   <script src="js/sparkline-chart.js"></script>
+   <script src="js/home-page-calender.js"></script>
+   <script src="js/home-chartjs.js"></script>
+
+   <!-- END JAVASCRIPTS -->   
+</body>
+<!-- END BODY -->
+</html>
--- administrator/includes/db.php
+++ administrator/includes/db.php
@@ -1,4 +1,4 @@
 <?php 
-$link = mysql_connect("localhost", "root", "Host@2017") or die("Error in Connection. Check Server Configuration.");
+$link = mysql_connect("localhost", "root", "abc123") or die("Error in Connection. Check Server Configuration.");
 mysql_select_db("roomrent", $link) or die("Database not Found. Please Create the Database.");
 ?>
